fdcba1be08523fd986ca1638c8f756fc5d8d3249
Spencer <Spencer.Taber@yahoo.com>
2021-04-07 21:46:24 +0100

    Code along completed

---
+++ src/compare.py
@@ -0,0 +1,9 @@
+def compare(num_1, num_2):
+    if num_1 < num_2:
+        return f"{num_1} is less than {num_2}"
+
+    elif num_1 == num_2:
+        return "Both numbers are equal"
+
+    return f"{num_1} is greater than {num_2}"
+
